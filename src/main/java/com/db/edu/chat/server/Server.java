27d086ed9b28bf4cdcd52befac4a599c6ee8e34d
eugene-krivosheyev <eugene.krivosheyev@gmail.com>
2014-03-31 17:44:22 +0400

    j.u.c.COWAL used

--- src/main/java/com/db/edu/chat/server/Server.java
+++ src/main/java/com/db/edu/chat/server/Server.java
@@ -15,7 +15,7 @@ public class Server {
 	public static final String HOST = "127.0.0.1";
 	public static final int PORT = 1235;
 	
-	private final Collection<Socket> clientsSockets = new java.util.ArrayList<Socket>();//new LinkedList<Socket>();
+	private final Collection<Socket> clientsSockets = new java.util.concurrent.CopyOnWriteArrayList<Socket>();
 	private boolean stopFlag;
 	private ServerSocket serverSocket;
 	private Thread connectionEventLoop = new Thread() {
