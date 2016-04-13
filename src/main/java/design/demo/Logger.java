d77dd420f728b9ac44f1669d03b52bfefa0327be
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2016-04-13 12:17:39 +0300

    Demo OOP step #1: pattern Creator.

--- src/main/java/design/demo/Logger.java
+++ src/main/java/design/demo/Logger.java
@@ -1,8 +1,8 @@
 package design.demo;
 
 public class Logger {
-    private LeveledLoggerFilter loggerFilter;
-    private ConsoleLoggerAppender loggerAppender;
+    private LeveledLoggerFilter loggerFilter = new LeveledLoggerFilter();
+    private ConsoleLoggerAppender loggerAppender = new ConsoleLoggerAppender();
 
     public void log(String message, int level) {
         if (loggerFilter.filter(message, level)) {
@@ -13,6 +13,6 @@ public class Logger {
 
 class LoggerTest {
     public static void main(String[] args) {
-        new Logger().log("message", 0);
+        new Logger().log("message", 3);
     }
 }
