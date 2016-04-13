839e3fef9f313f959e9a53679a99e34a2b62a1c1
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2016-04-13 11:49:08 +0300

    Demo procedure style.

--- src/main/java/design/demo/Logger.java
+++ src/main/java/design/demo/Logger.java
@@ -1,20 +1,26 @@
 package design.demo;
 
 public class Logger {
-    public void log(String message) {
-        System.out.println(message);
-    }
-
-    public void log(String... messages) {
-        for (String message : messages) {
-            log(message);
+    public void log(String message, int level) {
+        if (filter(message, level)) {
+            append(message, level);
         }
     }
+
+    //region Implementation
+    private void append(String message, int level) {
+
+    }
+
+    private boolean filter(String message, int level) {
+        return false;
+    }
+    //endregion
 }
 
 class LoggerTest {
     public static void main(String[] args) {
         Logger logger = new Logger();
-        logger.log("");
+        logger.log("message", 0);
     }
 }
