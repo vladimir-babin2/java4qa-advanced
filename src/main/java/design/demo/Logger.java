75e102af3cfc8e06ea9e35cc78ea0e2055e864ba
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2016-04-13 11:49:46 +0300

    Demo more procedure style.

--- src/main/java/design/demo/Logger.java
+++ src/main/java/design/demo/Logger.java
@@ -1,18 +1,18 @@
 package design.demo;
 
 public class Logger {
-    public void log(String message, int level) {
+    public static void log(String message, int level) {
         if (filter(message, level)) {
             append(message, level);
         }
     }
 
     //region Implementation
-    private void append(String message, int level) {
+    private static void append(String message, int level) {
 
     }
 
-    private boolean filter(String message, int level) {
+    private static boolean filter(String message, int level) {
         return false;
     }
     //endregion
@@ -20,7 +20,6 @@ public class Logger {
 
 class LoggerTest {
     public static void main(String[] args) {
-        Logger logger = new Logger();
-        logger.log("message", 0);
+        Logger.log("message", 0);
     }
 }
